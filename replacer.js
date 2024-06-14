5d797d2ee0e91e7d404ad0df30e977549808af7d
Petr Kuzora <petr1725@yandex.ru>
2024-06-14 11:07:22 +0300

    Deploy

--- replacer.js
+++ replacer.js
@@ -2,7 +2,7 @@ const replace = require("replace-in-file");
 const options = {
   //you may need to modify the file address to suite your project
   files: "./dist/_next/static/css/*.css",
-  from: ["_next/static"],
+  from: [/_next\/static/g],
   to: [".."],
 };
 (async function () {
