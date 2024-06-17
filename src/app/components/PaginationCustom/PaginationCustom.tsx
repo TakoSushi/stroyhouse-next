bd6179332e35e3cec040b40bdcea757adde0b594
Petr Kuzora <petr1725@yandex.ru>
2024-06-17 15:55:11 +0300

    Исправленна ошибка при сборке

--- src/app/components/PaginationCustom/PaginationCustom.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-
-import { useEffect, useState } from 'react';
-import ReactPaginate from 'react-paginate';
-import styles from './styles/index.module.css';
-
-export function PaginationCustom() {
- 
-}
